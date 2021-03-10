e7f8dbc69bc968b604176c65cfcc82cc0b1994a0
gmarquesaj <gmarquesaj@gmail.com>
2021-03-10 02:10:45 -0300

    Update pos_install.sh
    adicionado o download e permissão de exec do lmms e godot

--- pos_install.sh
+++ pos_install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+# Instalando repositorios
 sudo add-apt-repository ppa:lutris-team/lutris
 sudo add-apt-repository ppa:damentz/liquorix
 sudo add-apt-repository ppa:graphics-drivers/ppa
 #sudo ubuntu-drivers autoinstall -y
+#
 sudo apt install nvidia-driver-440 -y
 sudo apt install nvidia-cuda-toolkit -y
 sudo apt install gdebi -y
@@ -38,16 +41,27 @@ sudo snap install youtube-dl
 sudo snap install snap-store
 sudo snap install flameshot
 sudo snap install sublime-text --classic
+#
 mkdir pg
 cd pg
 wget -c https://www.tonelib.net/download/ToneLib-GFX-amd64.deb
-cd ..
+wget -c https://downloads.tuxfamily.org/godotengine/3.2.3/Godot_v3.2.3-stable_x11.64.zip
+wget -c https://github.com/LMMS/lmms/releases/download/v1.2.2/lmms-1.2.2-linux-x86_64.AppImage
+wget -c https://github.com/LMMS/lmms/releases/download/v1.3.0-alpha.1/lmms-1.3.0-alpha.1.33%2Bg4f74151-linux-x86_64.AppImage
+wget https://download.virtualbox.org/virtualbox/6.1.18/virtualbox-6.1_6.1.18-142142~Ubuntu~eoan_amd64.deb
+wget -c https://download.virtualbox.org/virtualbox/6.1.18/Oracle_VM_VirtualBox_Extension_Pack-6.1.18.vbox-extpack
 
+
+unzip Godot_v3.2.3-stable_x11.64.zip
+chmod +x *.AppImage
+rm Godot_v3.2.3-stable_x11.64.zip
+sudo dpkg -i *.deb
+
+cd ..
+#
 sudo apt-get install linux-image-liquorix-amd64 linux-headers-liquorix-amd64 -y
 echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list && wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key add -
 sudo apt update && sudo apt install linux-xanmod -y
-
-
 
 git clone --recurse-submodules https://github.com/flightlessmango/MangoHud.git
 cd MangoHud
