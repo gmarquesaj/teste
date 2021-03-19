f4be1854758c334440e36fcf7c390291bf04fc9e
gmarquesaj <gmarquesaj@gmail.com>
2021-03-19 01:52:21 -0300

    Update pos_install.sh

--- pos_install.sh
+++ pos_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Instalando repositorios
-sudo add-apt-repository ppa:lutris-team/lutris
-sudo add-apt-repository ppa:damentz/liquorix
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo add-apt-repository ppa:alexlarsson/flatpak
-#sudo ubuntu-drivers autoinstall -y
+sudo add-apt-repository ppa:lutris-team/lutris -y
+sudo add-apt-repository ppa:damentz/liquorix -y
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
+sudo add-apt-repository ppa:alexlarsson/flatpak -y
+sudo add-apt-repository ppa:flexiondotorg/mangohud -y
 #
 sudo apt install nvidia-driver-460 -y
 sudo apt install nvidia-cuda-toolkit -y
@@ -13,6 +13,7 @@ sudo apt install gdebi -y
 sudo apt install gedit -y
 sudo apt install lutris -y
 sudo apt install lazarus -y
+sudo apt-get install goverlay -y
 sudo apt install blender -y
 sudo apt install gimp -y
 sudo apt install flamerobin -y
@@ -40,16 +41,15 @@ sudo apt install jstest-gtk -y
 sudo apt install openjdk-11-jdk -y
 sudo apt install adb -y
 sudo apt install fastboot -y
-sudo snap install android-studio --classic
-sudo snap install code --classic
-sudo snap install telegram-desktop
-sudo snap install obs-studio
-sudo snap install vlc
-sudo snap install chromium
-sudo snap install youtube-dl
-sudo snap install snap-store
-sudo snap install flameshot
-sudo snap install sublime-text --classic
+#
+sudo nvidia-xconfig --cool-bits=28
+#
+sudo swapoff /swapfile
+sudo rm /swapfile
+sudo fallocate -l 8G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
 #
 mkdir pg
 cd pg
@@ -61,7 +61,6 @@ wget -c https://github.com/LMMS/lmms/releases/download/v1.2.2/lmms-1.2.2-linux-x
 wget -c https://github.com/LMMS/lmms/releases/download/v1.3.0-alpha.1/lmms-1.3.0-alpha.1.33%2Bg4f74151-linux-x86_64.AppImage
 wget -c https://download.virtualbox.org/virtualbox/6.1.18/Oracle_VM_VirtualBox_Extension_Pack-6.1.18.vbox-extpack
 
-
 unzip Godot_v3.2.3-stable_x11.64.zip
 chmod +x *.AppImage
 rm Godot_v3.2.3-stable_x11.64.zip
@@ -72,28 +71,22 @@ cd ..
 sudo apt-get install linux-image-liquorix-amd64 linux-headers-liquorix-amd64 -y
 echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list && wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key add -
 sudo apt update && sudo apt install linux-xanmod -y
+#
+pip3 install cython
+pip3 install virtualenv
 
-git clone --recurse-submodules https://github.com/flightlessmango/MangoHud.git
-cd MangoHud
-./build.sh build
-./build.sh package
-./build.sh install
-cd ..
-rm -rf MangoHud
-
-git clone https://github.com/DadSchoorse/vkBasalt.git
-cd vkBasalt
-meson --buildtype=release --prefix=/usr builddir
-ninja -C builddir install
-cd ..
-rm -rf vkBasalt
-
-git clone https://github.com/benjamimgois/goverlay.git
-cd goverlay
-lazbuild -B goverlay.lpi
-sudo mv goverlay /usr/games/
-cd ..
-rm -rf goverlay
+#
+sudo snap install android-studio --classic
+sudo snap install code --classic
+sudo snap install telegram-desktop
+sudo snap install obs-studio
+sudo snap install vlc
+sudo snap install chromium
+sudo snap install youtube-dl
+sudo snap install snap-store
+sudo snap install flameshot
+sudo snap install sublime-text --classic
+#
 
 
 
